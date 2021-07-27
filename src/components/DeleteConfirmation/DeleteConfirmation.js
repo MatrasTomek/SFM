553f76f826e355b80b2a6fa43d2e27b03114fa6b
DawidWebb <tomasz.matras@developerweb.pl>
2021-07-27 13:46:39 +0200

    add DeleteConfirmation component, add helpers for request and countries.

---
+++ src/components/DeleteConfirmation/DeleteConfirmation.js
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import { deleteSubcontractor } from "../../data/actions";
+import {} from "../../data/actions";
+
+import { Button, Modal } from "../../components";
+import styles from "./deleteConfirmation.module.scss";
+
+const DeleteConfirmation = ({
+  isModalOpen,
+  setIsModalOpen,
+  id,
+  setIsSave,
+  setIsEdit,
+  deleteItem,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleOnDelete = () => {
+    dispatch(deleteSubcontractor(id));
+    setIsModalOpen(false);
+    setIsSave(false);
+    setIsEdit(false);
+  };
+  const handleOnClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <Modal isModalOpen={isModalOpen}>
+      <div className={styles.wrapper}>
+        <p>Czy na pewno chcesz usunać?</p>
+        <div className={styles.buttons}>
+          <Button name="usuń" onClick={handleOnDelete} />
+          <Button name="wyjdź" onClick={handleOnClose} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteConfirmation;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as AsideMenu } from "./AsideMenu";
+export { Button, BackButton } from "./Buttons";
+export { default as BoxItem } from "./BoxItem";
+export { default as DeleteConfirmation } from "./DeleteConfirmation";
+export { Fleet, Info } from "./DataItems";
+export { default as MainSwitch } from "./MainSwitch";
+export { default as Modal } from "./Modal";
+export { default as Spinner } from "./Spinner";
+export { default as TaskInformation } from "./TaskInformation";
